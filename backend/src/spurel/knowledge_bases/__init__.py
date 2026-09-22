41af268a7d01c1982441c8e46355e5b683dc78f1
Summiya <summiya.rasheed@gmail.com>
2026-09-23 00:55:29 +0400

    feat: expose Knowledge Base application boundaries

--- backend/src/spurel/knowledge_bases/__init__.py
+++ backend/src/spurel/knowledge_bases/__init__.py
@@ -1,5 +1,16 @@
 """Knowledge base domain."""
 
 from spurel.knowledge_bases.domain import KnowledgeBase, KnowledgeBaseNameError
+from spurel.knowledge_bases.ports import (
+    KnowledgeBasePersistenceError,
+    KnowledgeBaseRepository,
+)
+from spurel.knowledge_bases.service import KnowledgeBaseService
 
-__all__ = ["KnowledgeBase", "KnowledgeBaseNameError"]
+__all__ = [
+    "KnowledgeBase",
+    "KnowledgeBaseNameError",
+    "KnowledgeBasePersistenceError",
+    "KnowledgeBaseRepository",
+    "KnowledgeBaseService",
+]
